0f818b0898d77ed5564ddcf8c4f40fa6f6c76c4b
famasboy888 <famasboy88@gmail.com>
2024-08-17 17:25:46 +0800

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import BACKEND_URL from "./utils/backend.url";
+
+function App() {
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    fetchMessage();
+  }, []);
+
+  const fetchMessage = async () => {
+    try {
+      const res = await fetch(`${BACKEND_URL}/api/test`);
+
+      const data = await res.json();
+
+      setMessage(data.message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Data from backend: {message}
+    </h1>
+  );
+}
+
+export default App;
